88441633ee0d5b11e9ba433dff707e3a51bc9511
rhymespearls <akuokojeremiah06@gmail.com>
2024-06-29 22:50:11 -0600

    new code

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,19 @@
-using System;
-
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        var scripture = new Scripture("Alma 32:32", "Therefore, if a seed groweth it is good, but if it groweth not, behold it is not good, therefore it is cast away.");
+
+        while (!scripture.IsVerseDone())
+        {
+           scripture.Display();
+                Console.WriteLine("Press Enter to continue or type 'Quit' to exit.");
+            var input = Console.ReadLine();
+
+            if (input == "quit")
+                break;
+
+            scripture.HideRandomWord();
+        }
     }
 }
